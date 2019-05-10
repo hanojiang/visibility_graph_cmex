2b3118f5fff642894677383bd15afb7683e34056
hanojiang <whj773768856@gmail.com>
2019-05-10 22:47:14 +0800

    add files about findVgEdge help m file

---
+++ findVgEdge.m
@@ -0,0 +1,5 @@
+function [left_node, right_node, weight]=findVgEdge(time_series)
+%% [left_node, right_node, weight]=findVgEdge(time_series) 
+% input time_series is a Nx1 array 
+% output: one connect edge between left_node and right_node,and the abs() weight of edge is weight. 
+end
